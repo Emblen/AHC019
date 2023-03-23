3c6c902230d869ea36367ca9c1af75d2c3664b99
Emblen <89460730+Kemblen@users.noreply.github.com>
2023-03-23 21:49:49 +0900

    提出1回目

--- main/submit1.cpp
+++ main/submit1.cpp
@@ -17,8 +17,8 @@ using namespace std;
 
 using namespace std;
 
-string inputfile = "seed000-05in.txt";
-string outputfile = "seed000-05out.txt";
+string inputfile = "seed001-08in.txt";
+string outputfile = "seed001-08out.txt";
 
 struct vec3{
     int x, y, z;
@@ -30,58 +30,81 @@ struct vec3{
 struct Solver{
     int d;
     vector<vector<vector<int>>> silhouette;
-    vector<vector<vector<bool>>> isblock;
+    vector<vector<vector<bool>>> isblock0, isblock1;
     vector<vector<int>> ans;
     vector<int> blocknum;
     Solver(int d, vector<vector<vector<int>>> silhouette) 
-    : d(d), silhouette(silhouette), ans(vector<vector<int>>(2)), blocknum({0,0}), isblock(d, vector<vector<bool>>(d, vector<bool>(d, false))) { }
+    : d(d), silhouette(silhouette), ans(vector<vector<int>>(2)), blocknum({0,0}), isblock0(d, vector<vector<bool>>(d, vector<bool>(d, false))), isblock1(d, vector<vector<bool>>(d, vector<bool>(d, false))) { }
 
     void solve(){
-        for(int sil=0; sil<2; sil++){
-            int n=1;
-            int f = sil*2, r = sil*2+1;
-            for(int x=0; x<d; x++){
-                for(int y=0; y<d; y++){
-                    for(int z=0; z<d; z++){
-                        if(silhouette[f][z][x]*silhouette[r][z][y]){
-                            ans[sil].push_back(n);
-                            n++;
-                        }
-                        else ans[sil].push_back(0);
-                    }
-                }
-            }
-            blocknum[sil] = n;
-        }
+        canexist(0, isblock0);
+        canexist(1, isblock1);
+
+        destruct(0, isblock0);
+        destruct(1, isblock1);
+
         ansout();
         return;
     }
 
+    //ブロックを置けるかどうか
+    void canexist(int sil, vector<vector<vector<bool>>>& isblock){
+        int f = sil*2, r = sil*2+1;
+        for(int x=0; x<d; x++){
+            for(int y=0; y<d; y++){
+                for(int z=0; z<d; z++){
+                    if(silhouette[f][z][x]*silhouette[r][z][y]){
+                        isblock[x][y][z] = true;
+                    }
+                }
+            }
+        }
+    }
+
     void ansout(){
 ///Local
-        ofstream FILEOUT(outputfile);
-        FILEOUT << max(blocknum[0], blocknum[1]) << endl;
-        for(int i=0; i<2; i++){
-            for(auto b:ans[i]) FILEOUT << b << " ";
-            FILEOUT << endl;
-        }
-//Submit
-        // cout << max(blocknum[0], blocknum[1]) << endl;
+        // ofstream FILEOUT(outputfile);
+        // FILEOUT << max(blocknum[0], blocknum[1]) << endl;
         // for(int i=0; i<2; i++){
         //     for(auto b:ans[i]) FILEOUT << b << " ";
-        //     cout << endl;
+        //     FILEOUT << endl;
         // }
+//Submit
+        cout << max(blocknum[0], blocknum[1]) << endl;
+        for(int i=0; i<2; i++){
+            for(auto b:ans[i]) cout << b << " ";
+            cout << endl;
+        }
 
         return;
     }
 
-    void destruct(){
-        vector<vector<bool>> zy_projection(d, vector<bool>(d, false)), zx_projectioin(d, vector<bool>(d, false));
-        for(int sil=0; sil<2; sil++){
-            
-        }
-    }
+    //不要なブロックを削る
+    void destruct(int sil, vector<vector<vector<bool>>>& isblock){
+        vector<vector<bool>> zy_projection(d, vector<bool>(d, false)), zx_projection(d, vector<bool>(d, false));
+        int n=0;
+        for(int x=0; x<d; x++){
+            for(int y=0; y<d; y++){
+                for(int z=0; z<d; z++){
+                    if(isblock[x][y][z]){
+                        if(zx_projection[z][x] && zy_projection[z][y]){
+                            isblock[x][y][z] = false;
+                            ans[sil].push_back(0);
+                            continue;
+                        }
 
+                        zx_projection[z][x] = true;
+                        zy_projection[z][y] = true;
+                        
+                        n++;
+                        ans[sil].push_back(n);
+                    }
+                    else ans[sil].push_back(0);
+                }
+            }
+        }
+        blocknum[sil] = n;
+    }
 };
 
 
@@ -91,13 +114,27 @@ int main(){
     start = chrono::system_clock::now(); // 計測開始時間
 
 ///Local
-    ifstream FILEIN(inputfile);
-    int d; FILEIN >> d;
+    // ifstream FILEIN(inputfile);
+    // int d; FILEIN >> d;
+    // vector<vector<vector<int>>> silhouette(4, vector<vector<int>>(d, vector<int>(d)));
+    // for(int sil=0; sil<4; sil++){
+    //     for(int i=0; i<d; i++){
+    //         for(int j=0; j<d; j++){
+    //             char a; FILEIN >> a;
+    //             silhouette[sil][i][j] = a-'0';
+    //         }
+    //     }
+    // }
+    // Solver solver(d, silhouette);
+    // solver.solve();
+
+///Submit
+    int d; cin >> d;
     vector<vector<vector<int>>> silhouette(4, vector<vector<int>>(d, vector<int>(d)));
     for(int sil=0; sil<4; sil++){
         for(int i=0; i<d; i++){
             for(int j=0; j<d; j++){
-                char a; FILEIN >> a;
+                char a; cin >> a;
                 silhouette[sil][i][j] = a-'0';
             }
         }
@@ -105,21 +142,9 @@ int main(){
     Solver solver(d, silhouette);
     solver.solve();
 
-///Submit
-    // int d; cin >> d;
-    // vector<vector<vector<int>>> silhouette(4, vector<vector<int>>(d, vector<int>(d)));
-    // for(int sil=0; sil<4; sil++){
-    //     for(int i=0; i<d; i++){
-    //         for(int j=0; j<d; j++){
-    //             char a; cin >> a;
-    //             silhouette[sil][i][j] = a-'0';
-    //         }
-    //     }
-    // }
-
-    cout << "#finished" << endl;
     end = chrono::system_clock::now();  // 計測終了時間
     double elapsed = chrono::duration_cast<chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
-    cout << "#Time: " << elapsed << " ms" << endl;
+    // cout << "#finished" << endl;
+    // cout << "#Time: " << elapsed << " ms" << endl;
     return 0;
 }
